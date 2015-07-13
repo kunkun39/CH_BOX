8ad89061551acf1943a3354b1bfa227224a49814
kunkun39 <34445282@qq.com>
2015-07-13 16:16:38 +0800

    * add comment

--- tv_assistant/TVhelper/main/src/com/changhong/tvhelper/view/SearchPageDefault.java
+++ tv_assistant/TVhelper/main/src/com/changhong/tvhelper/view/SearchPageDefault.java
@@ -36,43 +36,56 @@ import com.changhong.tvhelper.R;
 
 public class SearchPageDefault extends Fragment{
 	
+	// 总共的条数
 	private static final int MAX_ITEM_COUNT = 6;
+	
+	//用来测试的短语
 	private static final String TEST_WORDS = "abcd...";
 	
+	//当前的View
 	View view = null;
+	
+	// 动态创建的内部View
 	LinearLayout contentView = null;
+	
+	// 相关数据库
 	DatabaseContainer database =  MyApplication.databaseContainer;
 	
+	// 短语数据集
 	private Map<String,Integer> values = new LinkedHashMap<String, Integer>();	
+	
+	// 每一行布局
 	private List<LinearLayout> layouts = new LinkedList<LinearLayout>();
+	
+	// item的集合
 	private Map<View,LinearLayout> views = new HashMap<View, LinearLayout>();
-	Rect viewRect = new Rect();
+	
+	// 绑定的activity
 	Activity activity = null;
+	
+	/**
+	 * 函数重写
+	 * =================================================================================================================
+	 */
+	// 当绘制View的时候才将界面构建出来
 	@Override
 	public void onAttach(Activity activity) {
+		super.onAttach(activity);
 		this.activity = activity;
 		View view = activity.findViewById(R.id.search_page_content);
-		view.getViewTreeObserver().addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
-			
+		view.getViewTreeObserver().addOnGlobalLayoutListener(new OnGlobalLayoutListener() {		
 			@Override
 			public void onGlobalLayout() {
-				// TODO 自动生成的方法存根
 				if(SearchPageDefault.this.activity != null && !SearchPageDefault.this.view.isLayoutRequested())
 				{
-//					View view = SearchPageDefault.this.activity.findViewById(R.id.search_page_content);
-//					view.getLocalVisibleRect(viewRect);	
 					addViews(SearchPageDefault.this.contentView);	
 				}
-				getView().getViewTreeObserver().removeGlobalOnLayoutListener(this);
-				
+				getView().getViewTreeObserver().removeGlobalOnLayoutListener(this);				
 			}
-		});
-		
-		
-		super.onAttach(activity);
-		
+		});					
 	}
 		
+	//初始化View
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -90,15 +103,10 @@ public class SearchPageDefault extends Fragment{
 		contentView  = (LinearLayout)view.findViewById(R.id.search_idle_content);		
 		values.clear();
 		updateDatabase(activity);				
-		return view;//super.onCreateView(inflater, container, savedInstanceState);
+		return view;
 	}
-
-	@Override
-	public void onResume() {
-		// TODO 自动生成的方法存根
-		super.onResume();
-		
-	}
+	
+	// 添加View到根目录，只调用这个函数就能构建界面
 	private void addViews(LinearLayout view)
 	{
 		view.removeAllViews();
@@ -114,8 +122,94 @@ public class SearchPageDefault extends Fragment{
 		
 	}
 	
+	/**
+	 * 数据库操作
+	 * ===========================================================================================================
+	 */
+	//保存一个短语
+	public boolean saveSentences(Activity activity,String text)
+	{
+		boolean isSuccess = true;
+		if (database == null) {
+			database = new DatabaseContainer(activity);
+		}
+		
+		SQLiteDatabase sqLiteDatabase = database.getWritableDatabase();
+		sqLiteDatabase.beginTransaction();
+		try {		
+			String sqlString = "SELECT * FROM " + DatabaseContainer.TABLE_NAME_SEARCH_HEAT + " WHERE search_name = ?";
+			
+			Cursor cursor = sqLiteDatabase.rawQuery(sqlString, new String[]{text});
+			if (cursor.isAfterLast()) {
+				sqlString = "INSERT INTO  " + DatabaseContainer.TABLE_NAME_SEARCH_HEAT 
+						+ " ( search_name, search_time, search_count )" 
+						+ " VALUES( ?, DATE('NOW'), 1)";				
+				sqLiteDatabase.execSQL(sqlString, new String[]{text});
+			}
+			else {
+				sqlString = "UPDATE " + DatabaseContainer.TABLE_NAME_SEARCH_HEAT 
+						+ " SET search_time=DATE('NOW'), search_count=search_count+1" 
+						+ " WHERE search_name=?";
+				sqLiteDatabase.execSQL(sqlString, new String[]{text});
+			}
+			sqLiteDatabase.setTransactionSuccessful();
+		} catch (Exception e) {
+			e.printStackTrace();	
+			isSuccess = false;
+		}finally
+		{
+			sqLiteDatabase.endTransaction();
+		}
+		
+		return isSuccess;
+	}
+	
+	//加载短语
+	public Map<String,Integer> loadSentences(Activity activity)
+	{
+		String sqlString = "SELECT * FROM " 
+				+ DatabaseContainer.TABLE_NAME_SEARCH_HEAT 
+				+ " ORDER BY search_count DESC";
+		
+		values.clear();
+		if (database == null) {
+			database = new DatabaseContainer(activity);
+		}
+		Cursor cursor = database.getReadableDatabase().rawQuery(sqlString, null);
+		int count = MAX_ITEM_COUNT;		
+		
+		while (cursor.moveToNext()
+				&&((count--) > 0)) {
+			values.put(cursor.getString(0), cursor.getInt(2));					
+		}
+		return values;
+	}
+	
+	//删除一条短语
+	public void deleteSentences(Activity activity,String sentence)
+	{
+		String sqlString = "DELETE FROM " + DatabaseContainer.TABLE_NAME_SEARCH_HEAT + " WHERE search_name=?";
+		if (database == null) {
+			database = new DatabaseContainer(activity);
+		}
+		database.getReadableDatabase().execSQL(sqlString, new Object[]{sentence});		
+	}
+	
+	//删除大于一个月的短语
+	public void updateDatabase(Activity activity)
+	{
+		String sqlString = "DELETE FROM " + DatabaseContainer.TABLE_NAME_SEARCH_HEAT + " WHERE search_time BETWEEN 'NOW' AND DATE('-30 DAY')";
+		if (database == null) {
+			database = new DatabaseContainer(activity);
+		}
+		database.getReadableDatabase().execSQL(sqlString, new Object[]{});
+	}
 	
 	
+	/** 
+	 * 私有函数
+	 * ========================================================================================================
+	 */
 	private boolean addItemIntoLayout(View v)
 	{
 		LinearLayout layout;
@@ -123,6 +217,7 @@ public class SearchPageDefault extends Fragment{
 		View testView = generalItem(activity, TEST_WORDS);
 		int testWidth = getViewRect(testView).width();
 		
+		//当是第一条的时候直接添加进去
 		if (layouts.size() == 0) {
 			layout = generalLayout(activity);
 			if(getViewRect(v).width() + testWidth > view.getWidth())
@@ -140,6 +235,7 @@ public class SearchPageDefault extends Fragment{
 			return true;
 		}
 		
+		//获取一条已有的行，然后把现在的view加进去，否则就添加一条新行，加进去
 		layout =  layouts.get(layouts.size() -1);
 		int spaceLeft = view.getWidth();
 		for (Entry<View, LinearLayout> entry : views.entrySet()) {
@@ -187,6 +283,7 @@ public class SearchPageDefault extends Fragment{
 		return true;
 	}
 	
+	//获取一个view的Rect
 	private Rect getViewRect(View v)
 	{
 		Rect rect = new Rect();//v.getClipBounds();
@@ -195,6 +292,7 @@ public class SearchPageDefault extends Fragment{
 		return rect;
 	}
 	
+	// 添加一个短语
 	private View addSentence(String Sentence)
 	{
 		String temp = Sentence;
@@ -226,6 +324,7 @@ public class SearchPageDefault extends Fragment{
 		return null;
 	}
 	
+	//生成一个layout
 	private LinearLayout generalLayout(Context context)
 	{		
 		
@@ -237,6 +336,7 @@ public class SearchPageDefault extends Fragment{
 		return layout;
 	}
 	
+	// 生成一个item
 	private View generalItem(Context context,String text) {
 		final TextView view = new TextView(context);
 		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
@@ -247,8 +347,8 @@ public class SearchPageDefault extends Fragment{
 		view.setEllipsize(TruncateAt.END);
 		view.setPadding(20, 10, 20, 10);
 		view.setGravity(Gravity.CENTER);
+		//点击的时候搜索
 		view.setOnClickListener(new OnClickListener() {
-			
 			@Override
 			public void onClick(View v) {
 				Button searchButton = (Button) activity.findViewById(R.id.btn_search);
@@ -257,13 +357,14 @@ public class SearchPageDefault extends Fragment{
 				searchButton.performClick();
 			}
 		});
+		//长按删除
 		view.setOnLongClickListener(new OnLongClickListener() {
 			
 			@Override
 			public boolean onLongClick(View v) {
 				
 				AlertDialog dialog = new AlertDialog.Builder(activity)
-						.setMessage("确定要删除\n" + view.getText() + "?")
+						.setMessage("纭畾瑕佸垹闄n" + view.getText() + "?")
 						.setPositiveButton(android.R.string.yes, new Dialog.OnClickListener() {
 							
 							@Override
@@ -276,7 +377,7 @@ public class SearchPageDefault extends Fragment{
 							
 							@Override
 							public void onClick(DialogInterface dialog, int which) {
-								// TODO 自动生成的方法存根
+								// TODO 鑷姩鐢熸垚鐨勬柟娉曞瓨鏍�
 								dialog.dismiss();
 							}
 						})	
@@ -290,83 +391,6 @@ public class SearchPageDefault extends Fragment{
 		return view;
 	}
 	
-	public boolean saveSentences(Activity activity,String text)
-	{
-		boolean isSuccess = true;
-		if (database == null) {
-			database = new DatabaseContainer(activity);
-		}
-		
-		SQLiteDatabase sqLiteDatabase = database.getWritableDatabase();
-		sqLiteDatabase.beginTransaction();
-		try {		
-			String sqlString = "SELECT * FROM " + DatabaseContainer.TABLE_NAME_SEARCH_HEAT + " WHERE search_name = ?";
-			
-			Cursor cursor = sqLiteDatabase.rawQuery(sqlString, new String[]{text});
-			if (cursor.isAfterLast()) {
-				sqlString = "INSERT INTO  " + DatabaseContainer.TABLE_NAME_SEARCH_HEAT 
-						+ " ( search_name, search_time, search_count )" 
-						+ " VALUES( ?, DATE('NOW'), 1)";				
-				sqLiteDatabase.execSQL(sqlString, new String[]{text});
-			}
-			else {
-				sqlString = "UPDATE " + DatabaseContainer.TABLE_NAME_SEARCH_HEAT 
-						+ " SET search_time=DATE('NOW'), search_count=search_count+1" 
-						+ " WHERE search_name=?";
-				sqLiteDatabase.execSQL(sqlString, new String[]{text});
-			}
-			sqLiteDatabase.setTransactionSuccessful();
-		} catch (Exception e) {
-			e.printStackTrace();	
-			isSuccess = false;
-		}finally
-		{
-			sqLiteDatabase.endTransaction();
-		}
-		
-		return isSuccess;
-	}
 	
-	public Map<String,Integer> loadSentences(Activity activity)
-	{
-		String sqlString = "SELECT * FROM " 
-				+ DatabaseContainer.TABLE_NAME_SEARCH_HEAT 
-				+ " ORDER BY search_count DESC";
-		
-		values.clear();
-		if (database == null) {
-			database = new DatabaseContainer(activity);
-		}
-		Cursor cursor = database.getReadableDatabase().rawQuery(sqlString, null);
-		int count = MAX_ITEM_COUNT;		
-		
-		while (cursor.moveToNext()
-				&&((count--) > 0)) {
-			values.put(cursor.getString(0), cursor.getInt(2));					
-		}
-		
-		
-		
-		return values;
-	}
-	
-	public void deleteSentences(Activity activity,String sentence)
-	{
-		String sqlString = "DELETE FROM " + DatabaseContainer.TABLE_NAME_SEARCH_HEAT + " WHERE search_name=?";
-		if (database == null) {
-			database = new DatabaseContainer(activity);
-		}
-		database.getReadableDatabase().execSQL(sqlString, new Object[]{sentence});
-		
-	}
-	
-	public void updateDatabase(Activity activity)
-	{
-		String sqlString = "DELETE FROM " + DatabaseContainer.TABLE_NAME_SEARCH_HEAT + " WHERE search_time BETWEEN 'NOW' AND DATE('-30 DAY')";
-		if (database == null) {
-			database = new DatabaseContainer(activity);
-		}
-		database.getReadableDatabase().execSQL(sqlString, new Object[]{});
-	}
 
 }
