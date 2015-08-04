82f07a53bca45f54c437a97f6cd3312c77c857a8
kunkun39 <34445282@qq.com>
2015-08-04 11:45:01 +0800

    * fix bug: 卫视 convert to null

--- tv_assistant/TVhelper/main/src/com/changhong/tvhelper/utils/YuYingWordsUtils.java
+++ tv_assistant/TVhelper/main/src/com/changhong/tvhelper/utils/YuYingWordsUtils.java
@@ -318,7 +318,7 @@ public class YuYingWordsUtils {
         TV_CHANNEL_SEARCH_KEYWORDS.put("十三", "13");
         TV_CHANNEL_SEARCH_KEYWORDS.put("台", "");
         TV_CHANNEL_SEARCH_KEYWORDS.put("套", "");
-        TV_CHANNEL_SEARCH_KEYWORDS.put("卫视", "");
+        // Yves Yang:TV_CHANNEL_SEARCH_KEYWORDS.put("卫视", "");
         TV_CHANNEL_SEARCH_KEYWORDS.put("电视", "");
     }
 
