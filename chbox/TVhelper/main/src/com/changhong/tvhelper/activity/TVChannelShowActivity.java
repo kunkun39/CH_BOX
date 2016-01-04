37d6d659c45e3d1b156d38ce5b0f8cf558ace712
kunkun39 <34445282@qq.com>
2016-01-04 14:47:55 +0800

    * modify res

--- chbox/TVhelper/main/src/com/changhong/tvhelper/activity/TVChannelShowActivity.java
+++ chbox/TVhelper/main/src/com/changhong/tvhelper/activity/TVChannelShowActivity.java
@@ -499,7 +499,7 @@ public class TVChannelShowActivity extends Activity {
              * 收藏频道和取消收藏
              */
             if (allShouChangChannel.contains(channelServiceId)) {
-                vh.channelShouCang.setText(getString(android.R.string.cancel) + "\n" + getString(R.string.favorite));
+                vh.channelShouCang.setText(getString(R.string.delete) + "\n" + getString(R.string.favorite));
                 vh.channelShouCang.setTextColor(getResources().getColor(R.color.orange));
             } else {
                 vh.channelShouCang.setText(getString(R.string.favorite) + "\n" + getString(R.string.channel));
@@ -527,7 +527,7 @@ public class TVChannelShowActivity extends Activity {
                         //收藏操作
                         boolean success = channelService.channelShouCang(channelServiceId);
                         if (success) {
-                            vh.channelShouCang.setText(getString(R.string.cancel)+"\n" + getString(R.string.favorite));
+                            vh.channelShouCang.setText(getString(R.string.delete)+"\n" + getString(R.string.favorite));
                             vh.channelShouCang.setTextColor(getResources().getColor(R.color.orange));
                             allShouChangChannel.add(channelServiceId);
 
